b7bed517b4109ec3ce361ab3225644600f9e90f5
Ángel Caparrós Caballero <135209076+kubaliski@users.noreply.github.com>
2025-01-27 13:33:52 +0100

    refactor(viteurl): baseUrl now use .env variable

--- resources/js/services/api/httpClient.js
+++ resources/js/services/api/httpClient.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const baseConfig = {
-    baseURL: 'http://localhost:8000',
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
         'X-Requested-With': 'XMLHttpRequest'
     },
