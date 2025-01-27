510d26ac6a8470d16b078064500fb141374637fd
Ángel Caparrós Caballero <135209076+kubaliski@users.noreply.github.com>
2025-01-27 10:33:52 +0100

    add correct user to UserSeeder.php to production/wedplan

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,32 +13,12 @@ class UserSeeder extends Seeder
     {
         // Usuario Administrador
         $adminUser = User::create([
-            'name' => 'Admin User',
-            'last_name' => 'Admin',
-            'email' => 'admin@example.com',
-            'password' => Hash::make('password'),
+            'name' => 'Angel',
+            'last_name' => 'Caparros',
+            'email' => 'angel.ccapb@gmail.com',
+            'password' => Hash::make('45023PokE!'),
             'email_verified_at' => now(),
         ]);
         $adminUser->assignRole('admin');
-
-        // Usuario Editor
-        $editorUser = User::create([
-            'name' => 'Editor User',
-            'last_name' => 'Editor',
-            'email' => 'editor@example.com',
-            'password' => Hash::make('password'),
-            'email_verified_at' => now(),
-        ]);
-        $editorUser->assignRole('editor');
-
-        // Usuario Soporte
-        $supportUser = User::create([
-            'name' => 'Support User',
-            'last_name' => 'Support',
-            'email' => 'support@example.com',
-            'password' => Hash::make('password'),
-            'email_verified_at' => now(),
-        ]);
-        $supportUser->assignRole('support');
     }
 }
